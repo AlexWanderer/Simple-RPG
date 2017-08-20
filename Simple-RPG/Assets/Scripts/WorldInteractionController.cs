f9c1781868b792829de2738ec55de41ec9a68a61
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-20 19:19:16 +0200

    Added a new Controller Script
    Added a New Lighting System, and created a new World Interaction Controller. This Script handles the Players interaction within the Game World.

---
+++ Simple-RPG/Assets/Scripts/WorldInteractionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteractionController : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+
+    void GetInteraction()
+    {
+
+    }
+}
+
+
+/** 
+ * // Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+ * **/
