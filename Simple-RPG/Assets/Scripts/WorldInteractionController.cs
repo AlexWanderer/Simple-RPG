095e21a9596e95ba18f40310ee73c1063382c589
dale-smith <dale.smith.workapp01@gmail.com>
2017-08-21 11:24:40 +0200

    Added New Features.
    Created the Function for the Player to be able to move with their Right Mouse Button.
    
    Camera now follows the Player around.
    
    Created a NavMesh.

--- Simple-RPG/Assets/Scripts/WorldInteractionController.cs
+++ Simple-RPG/Assets/Scripts/WorldInteractionController.cs
@@ -1,30 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WorldInteractionController : MonoBehaviour {
+
+    NavMeshAgent playerAgent;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-
-        }
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            GetInteraction();
     }
 
     void GetInteraction()
     {
-
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactionObject = interactionInfo.collider.gameObject;
+            if (interactionObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable Object");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
     }
 }
-
-
-/** 
- * // Use this for initialization
-	void Start () {
-		
-	}
-	
-	
- * **/
